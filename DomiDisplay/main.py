0ff9f78a06aab608d977905508a2c40a4e012f2a
Visual GMQ <2142587070@qq.com>
2019-02-03 22:38:30 +0800

    added 极快速如梦教程.md

--- DomiDisplay/main.py
+++ DomiDisplay/main.py
@@ -8,12 +8,60 @@ app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
 app.config['DATABASE'] = ''
 
+#用于在数据库查询当中配合where句
+def decorateString(string):
+    if string == None:
+        return '"null"'
+    else:
+        return '"'+string+'"'
+
+
+'''
+数据库查找函数
+如果db为None，那么函数会通过你给出的参数来进行一次性查询（在内部打开和关闭数据库）。
+如果db为已经生成的数据库链接，那么查询之后不会关闭数据库。
+其中fetch参数决定cursor获得多少数据：
+    1：执行.fetchone()获取一条数据
+    >1：执行.fetchmany()获取多条数据
+    <1：执行.fetchall()获取所有数据
+'''
+def searchDB(db,table,condition,fetch=-1,cols='*',OrderBy=None,isDesc=False):
+    global flag
+    flag = False
+    if type(db) == str:
+        db = sqlite3.connect(app.config['DATABASE'] + db)
+        flag = True
+    elif db==None:
+        return None
+
+    cursor = db.cursor()
+    string = 'SELECT %s FROM %s WHERE %s'% (cols,table,condition)
+    if not OrderBy == None:
+        string += " ORDER BY %s" % OrderBy
+    if isDesc == True:
+        string += " DESC"
+    cursor.execute(string)
+
+    global info
+    if fetch<1:
+        info = cursor.fetchall()
+    elif fetch==1:
+        info = cursor.fetchone()
+    else:
+        info = cursor.fetchmany(fetch)
+    if flag == True:
+        db.close()
+    return info
+
 def doLogin(username, password):
+    '''
     db = sqlite3.connect(app.config['DATABASE']+"users.db")
     cursor = db.cursor()
     cursor.execute('SELECT * FROM users WHERE name=%s' % "'"+username+"'")
     info = cursor.fetchone()
     db.close()
+    '''
+    info = searchDB('users.db','users',"name=%s" % decorateString(username),fetch=1)
     if info == None:
         return False
     else:
@@ -36,26 +84,21 @@ def changeScore(dom,score):
         db.close()
         return True
 
-
-
 @app.route('/',methods=['POST','GET'])
 def homepage():
     db = sqlite3.connect(app.config['DATABASE']+"domis.db")
     cursor = db.cursor()
-    info = cursor.execute("SELECT name,score FROM domits ORDER BY score DESC").fetchall()
+    global info
+    if request.method == 'POST':
+        cursor.execute("SELECT score FROM domits WHERE name=%s" % '"'+request.form.get('searchdom', 'null')+'"')
+        searchinfo = cursor.fetchone()
+        info = [(request.form.get('searchdom','null'),searchinfo[0] if searchinfo!=None else 'NAN')]    #三目表达式
+    else:
+        info = cursor.execute("SELECT name,score FROM domits ORDER BY score DESC").fetchall()
+    db.close()
     return render_template('homepage.html',doms=info)
 
-@app.route('/login/',methods=['POST','GET'])
-def login():
-    if request.method == 'POST':
-        if doLogin(request.form.get('username','null'), request.form.get('password','null')) == True:
-            session['logined']=True
-            return redirect(url_for('modify'))
-        else:
-            session['logined']=False
-            flash('login failed')
 
-    return render_template('login.html')
 
 @app.route('/modify/',methods=['GET','POST'])
 def modify():
@@ -80,6 +123,18 @@ def modify():
         cursor.execute("SELECT name,score FROM domits ORDER BY name DESC")
         info = cursor.fetchall()
         return render_template('modify.html',doms=info)
+
+@app.route('/login/',methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        if doLogin(request.form.get('username','null'), request.form.get('password','null')) == True:
+            session['logined']=True
+            return redirect(url_for('modify'))
+        else:
+            session['logined']=False
+            flash('login failed')
+
+    return render_template('login.html')
 
 @app.route('/logout/',methods=['POST'])
 def logout():
